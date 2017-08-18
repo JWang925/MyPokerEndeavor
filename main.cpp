f59c6d9e7a6623fceadac76407348787e6459194
JWang925 <wangjiajun925@gmail.com>
2017-08-18 00:39:19 -0400

    recommit

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
+#pragma once
+
 #include<iostream>
 #include<string>
 #include"abc.h"
 using namespace std;
 
 int main(){
-
-PokerHand hand1("2H 3H 4H 5H 6H");
-PokerHand hand2("KS AS TS QS JS");
-
-Result out=compare(hand1,hand2);
-
-
-return 0;
+	PokerHand hand1("2H 3H 4H 5H 6D 7H 8H");
+	PokerHand hand2("KS AS TS QS JD 9S 8S");
+	Result out=compare(hand1,hand2);
+	cout<<out;
+	return 0;
 }
 
 
